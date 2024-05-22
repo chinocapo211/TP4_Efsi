347c655b49fed1a2c9aa315c087fb8f5dd057046
FELIPE BLAUSTEIN <47807182@ort>
2024-05-22 14:31:56 -0300

    a

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -4,6 +4,7 @@ function Contacto() {
   return (
     <div id="container4"> 
       <h1>Contactame!</h1>
+      <div id="contacto_todo">
       <div className="card-container">
         <div id="conta" className="card">
           <div className="card-front">
@@ -13,6 +14,13 @@ function Contacto() {
             <h3>felipe.blaustein@gmail.com</h3>
           </div>
         </div>
+      </div>
+      <form id="contact-form" method="post">
+              <input type="text" name="name" placeholder="Nombre" required />
+              <input type="email" name="email" placeholder="Correo electrónico" required />
+              <input type="text" name="message" placeholder="Mensaje" required></input>
+              <button type="submit">Enviar</button>
+        </form>
       </div>
     </div>
   );
